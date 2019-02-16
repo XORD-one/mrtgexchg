483e31d4dc9c7d5523fc6d1f70575c9db26cf8d6
Abdul Sami <sami-abdul@outlook.com>
2019-02-16 19:37:19 +0500

    Update invoke.js

--- application/invoke.js
+++ application/invoke.js
@@ -30,7 +30,7 @@ async function main() {
 
         // Create a new gateway for connecting to our peer node.
         const gateway = new Gateway();
-        await gateway.connect(ccp, { wallet, identity: 'Admin@Registry.com', discovery: { enabled: false, asLocalhost: true } });
+        await gateway.connect(ccp, { wallet, identity: 'Admin@Registry.com', discovery: { enabled: true, asLocalhost: true } });
 
         // Get the network (channel) our contract is deployed to.
         const network = await gateway.getNetwork('records');
@@ -42,9 +42,7 @@ async function main() {
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
 
-        // await contract.submitTransaction('createRealEstate','125', '5 High Strret, TX 75000', '250000', '4000 sq. ft 3 beds 2 baths blah blah', 'John Doe');
-        await contract.createTransaction("createRealEstate")
-            .submit('125', '5 High Strret, TX 75000', '250000', '4000 sq. ft 3 beds 2 baths blah blah', 'John Doe');
+        await contract.submitTransaction('createRealEstate','125', '5 High Strret, TX 75000', '250000', '4000 sq. ft 3 beds 2 baths blah blah', 'John Doe');
         console.log('Transaction has been submitted');
 
         // Disconnect from the gateway.
