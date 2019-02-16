d306170816cec4ae9547070c424a3c9a4c125df6
Abdul Sami <sami-abdul@outlook.com>
2019-02-16 18:51:25 +0500

    Update invoke.js

--- application/invoke.js
+++ application/invoke.js
@@ -41,7 +41,10 @@ async function main() {
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-        await contract.submitTransaction('createRealEstate','125', '5 High Strret, TX 75000', '250000', '4000 sq. ft 3 beds 2 baths blah blah', 'John Doe');
+
+        let args = ['125', '5 High Strret, TX 75000', '250000', '4000 sq. ft 3 beds 2 baths blah blah', 'John Doe'];
+        // await contract.submitTransaction('createRealEstate','125', '5 High Strret, TX 75000', '250000', '4000 sq. ft 3 beds 2 baths blah blah', 'John Doe');
+        await contract.submitTransaction('createRealEstate', args);
         console.log('Transaction has been submitted');
 
         // Disconnect from the gateway.
