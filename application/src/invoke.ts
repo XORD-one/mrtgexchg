311e559d3f3361ade0d6deca239d3fa3a98d22af
Abdul Sami <sami-abdul@outlook.com>
2019-02-18 15:50:15 +0500

    Update invoke.js

--- application/src/invoke.ts
+++ application/src/invoke.ts
@@ -6,7 +6,7 @@ import { FileSystemWallet, Gateway } from 'fabric-network';
 import * as fs from 'fs';
 import * as path from 'path';
 
-const ccpPath = path.resolve(__dirname, '..', '..', '..', 'basic-network', 'connection.json');
+const ccpPath = path.resolve(__dirname, '..', '..', 'connection.json');
 const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
 const ccp = JSON.parse(ccpJSON);
 
@@ -19,27 +19,26 @@ async function main() {
         console.log(`Wallet path: ${walletPath}`);
 
         // Check to see if we've already enrolled the user.
-        const userExists = await wallet.exists('user1');
+        const userExists = await wallet.exists('admin');
         if (!userExists) {
-            console.log('An identity for the user "user1" does not exist in the wallet');
-            console.log('Run the registerUser.ts application before retrying');
+            console.log('An identity for the admin does not exist in the wallet');
             return;
         }
 
         // Create a new gateway for connecting to our peer node.
         const gateway = new Gateway();
-        await gateway.connect(ccp, { wallet, identity: 'user1', discovery: { enabled: false } });
+        await gateway.connect(ccp, { wallet, identity: 'admin', discovery: { enabled: false } });
 
         // Get the network (channel) our contract is deployed to.
-        const network = await gateway.getNetwork('mychannel');
+        const network = await gateway.getNetwork('records');
 
         // Get the contract from the network.
-        const contract = network.getContract('fabcar');
+        const contract = network.getContract('recordschaincode');
 
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-        await contract.submitTransaction('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom');
+        await contract.submitTransaction('createRealEstate', '123', '5 High Strret, TX 75000', '250000', '4000 sq. ft 3 beds 2 baths blah blah', 'John Doe');
         console.log(`Transaction has been submitted`);
 
         // Disconnect from the gateway.
