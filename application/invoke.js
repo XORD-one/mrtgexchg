045fc4a40ec6d561652baa65e8291b5c19ac22b9
Abdul Sami <sami-abdul@outlook.com>
2019-02-18 20:21:35 +0500

    Update invoke.js

---
+++ application/invoke.js
@@ -0,0 +1,18 @@
+var hfc = require('fabric-client');
+let client = new hfc();
+
+let tx_object = client.newTransactionID();
+let tx_id = tx_object.getTransactionID();
+
+let request = {
+    targets: [],
+    chaincodeId: 'recordschaincode',
+    fcn: 'createRealEstate',
+    args: ["125", "5 High Strret, TX 75000", "250000", "4000 sq. ft 3 beds 2 baths blah blah", "John Doe"],
+    txId: tx_object
+};
+let channel = client.getChannel('records');
+return channel.sendTransactionProposal(request)
+    .then((results) => {
+        console.log('Successfully endorsed proposal to invoke chaincode');
+    });
