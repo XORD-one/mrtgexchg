71f7757aa087db45ba9b3a64fbb115dfeeebd102
Abdul Sami <sami-abdul@outlook.com>
2019-02-18 15:09:33 +0500

    Update start_network.sh

--- scripts/start_network.sh
+++ scripts/start_network.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-compose -f docker-compose-mrtgexchg.yaml up -d orderer.mrtgexchg.com peer0.Bank.com peer0.Insurance.com peer0.Registry.com peer0.Title.com peer0.Appraiser.com peer0.Audit.com peer0.Fico.com
+docker-compose -f docker-compose-mrtgexchg.yaml up -d ca.Registry.com orderer.mrtgexchg.com peer0.Bank.com peer0.Insurance.com peer0.Registry.com peer0.Title.com peer0.Appraiser.com peer0.Audit.com peer0.Fico.com
 docker-compose -f docker-compose-mrtgexchg.yaml up -d cli.Bank cli.Insurance cli.Registry cli.Title cli.Appraiser cli.Audit cli.Fico
