1c0aa69301c397ad1eefbde65b855fee3e0dc302
Abdul Sami <sami-abdul@outlook.com>
2019-02-19 11:43:38 +0500

    Update invoke.js

--- application/invoke.js
+++ application/invoke.js
@@ -9,10 +9,12 @@ let request = {
     chaincodeId: 'recordschaincode',
     fcn: 'createRealEstate',
     args: ["125", "5 High Strret, TX 75000", "250000", "4000 sq. ft 3 beds 2 baths blah blah", "John Doe"],
+    chainId: 'records',
     txId: tx_object
 };
 let channel = client.getChannel('records');
 return channel.sendTransactionProposal(request)
     .then((results) => {
+        console.log(results);
         console.log('Successfully endorsed proposal to invoke chaincode');
     });
